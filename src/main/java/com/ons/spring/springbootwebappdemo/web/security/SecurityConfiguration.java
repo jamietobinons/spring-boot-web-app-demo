f58f42c79af8c428ed9be928693c9240dae43ee1
Jamie <jamie.tobin@gmail.com>
2018-06-09 21:30:10 +0100

    Step 20
    Added Spring Security.
    Redirecting to the standard login form.

---
+++ src/main/java/com/ons/spring/springbootwebappdemo/web/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.ons.spring.springbootwebappdemo.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by tobinj on 09/06/2018.
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("test").password("test")
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login").permitAll()
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+                .formLogin();
+    }
+}
